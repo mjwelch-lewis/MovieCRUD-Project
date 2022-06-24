056dba3d0186e5a966e18a6328ba11594a801835
mjwelch-lewis <matthewjwelch@lewisu.edu>
2022-06-24 16:48:22 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,56 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'))
 
+// Start MongoDB Atlas ********
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mongoose = require("mongoose");
+const mongooseUri = "mongodb+srv://MovieCRUDUser:if5tSyX4Lf8zc9cW@moviedatabase.xe14l.mongodb.net/movieDatabase"
+
+mongoose.connect(mongooseUri, {useNewUrlParser: true}, {useUnifiedTopology: true})
+
+const movieSchema = {
+	title: String,
+	comments: String
+}
+
+const Movie = mongoose.model("movie", movieSchema);
+
+// Create route called from create.html
+app.post("/create", function(req, res){
+	let newNote = new Movie({
+		title: req.body.title,
+		comments: req.body.comments
+	})
+	
+	newNote.save();
+	res.redirect("/");
+})
+
+const renderMovies = (moviesArray) => {
+	let text = "Movies Collection:\n\n";
+	moviesArray.forEach((movie)=>{
+		text += "Title: " + movie.title  + "\n";
+		text += "Comments: " + movie.comments  + "\n";
+		text += "ID:" + movie._id + "\n\n";
+	})
+	text += "Total Count: " + moviesArray.length;
+	return text
+}
+
+app.get("/read", function(request, response) {
+	Movie.find({}).then(movies => { 
+		response.type('text/plain');
+		response.send(renderMovies(movies));
+	})
+})
+
+// Todo: Implement your own MongoDB Atlas Organization, Project, Database Cluster, Database, and Collection.
+// Todo: Implement and test the Update and Delete functionCRUD.
+
+// End MongoDB Atlas ********
+
 const port = process.env.PORT || 3000
 app.get('/test', function(request, response) {
 	response.type('text/plain')
